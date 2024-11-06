2b83cd45809bba9db8d45de36ac0e8f5c9251b6c
Šimon Munka <mcmunka@gmail.com>
2024-11-06 13:09:14 +0100

    rm unneeded files

--- src/app/provider.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
-
-export default function RootLayout(props: { children: React.ReactNode }) {
-  return (
-    <ChakraProvider value={defaultSystem}>{props.children}</ChakraProvider>
-  );
-}
